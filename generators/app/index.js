40379f68959953d515cd1582a9d37c000c8b10c9
Markus Falk <mail@markus-falk.com>
2018-07-23 18:13:31 +0200

    [FEATURE] add tests to new folder structure refs #1, #9

--- generators/app/index.js
+++ generators/app/index.js
@@ -49,7 +49,8 @@ module.exports = class extends Generator {
 
     return this.prompt(prompts).then(props => {
       this.props = props;
-      this.props.moduleName = _.camelCase(this.props.moduleName);
+      this.props.moduleNameCamelCased = _.camelCase(this.props.moduleName);
+      this.props.moduleName = this.props.moduleNameCamelCased;
       this.props.moduleName = _.upperFirst(this.props.moduleName);
       this.props.path = _.kebabCase(this.props.moduleName);
     });
@@ -90,6 +91,7 @@ module.exports = class extends Generator {
       }
     );
 
+    // Module
     this.fs.copyTpl(
       this.templatePath('./src/foo-module/foo.module.ts.template'),
       this.destinationPath(`./src/${this.props.path}/${this.props.path}.module.ts`),
@@ -100,9 +102,22 @@ module.exports = class extends Generator {
     );
 
     this.fs.copyTpl(
-      this.templatePath('./src/foo-module/foo-components/foo.component.ts.template'),
+      this.templatePath('./src/foo-module/foo.module.spec.ts.template'),
+      this.destinationPath(`./src/${this.props.path}/${this.props.path}.module.spec.ts`),
+      {
+        moduleName: this.props.moduleName,
+        moduleNameCamelCased: this.props.moduleNameCamelCased,
+        path: this.props.path
+      }
+    );
+
+    // Component
+    this.fs.copyTpl(
+      this.templatePath('./src/foo-module/foo-components/foo/foo.component.ts.template'),
       this.destinationPath(
-        `./src/${this.props.path}/components/${this.props.path}.component.ts`
+        `./src/${this.props.path}/components/${this.props.path}/${
+          this.props.path
+        }.component.ts`
       ),
       {
         moduleName: this.props.moduleName,
@@ -110,6 +125,22 @@ module.exports = class extends Generator {
       }
     );
 
+    this.fs.copyTpl(
+      this.templatePath(
+        './src/foo-module/foo-components/foo/foo.component.spec.ts.template'
+      ),
+      this.destinationPath(
+        `./src/${this.props.path}/components/${this.props.path}/${
+          this.props.path
+        }.component.spec.ts`
+      ),
+      {
+        moduleName: this.props.moduleName,
+        path: this.props.path
+      }
+    );
+
+    // Package.json
     this.fs.copyTpl(
       this.templatePath('package.json.template'),
       this.destinationPath(`package.json`),
